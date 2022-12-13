68018a8553c2c1680fd61eaa40c7aeedce272212
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2022-12-13 16:42:53 +0400

    bump version → 0.2.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_dev_required():
 
 setup(
     name="vedro-cloud",
-    version="0.2.0",
+    version="0.2.1",
     description="Validator Cloud plugin",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
--- vedro_cloud/__init__.py
+++ vedro_cloud/__init__.py
@@ -1,4 +1,4 @@
 from ._vedro_cloud_plugin import VedroCloud, VedroCloudPlugin
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __all__ = ("VedroCloud", "VedroCloudPlugin",)
